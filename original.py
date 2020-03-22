fe4fa99f50a4bd52138ee299c24abee71c8ac113
skanderbegf <47793257+amalo201@users.noreply.github.com>
2020-03-22 17:39:46 +0000

    Add files via upload
    added signal

--- original.py
+++ original.py
@@ -2,7 +2,7 @@
 
 import sys, os, signal
 from multiprocessing import Process
-
+import toolz
 from scapy.all import *
 
 interface = ' '  # the interface to be put in monitod mode
@@ -11,11 +11,12 @@ duplicates = []
 wname = []
 list_accesspoints= []
 class accesspoint_object:
-  def __init__(self, ssid, mac, channel, enc):
+  def __init__(self, ssid, mac, channel, enc, signal):
     self.ssid = ssid
     self.mac = mac
     self.channel = channel
     self.enc = enc
+    self.signal = signal
 
 
 
@@ -25,33 +26,24 @@ def FindAps(pkt) :
 			if pkt.addr2 not in aps:
 				aps.append(pkt.addr2)	
 				encryption = pkt.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}")
+
+				radiotap = pkt.getlayer(RadioTap)
+				signal = radiotap.dBm_AntSignal
 				if re.search("privacy", encryption): enc = 'Y'
 				else: enc = 'N'
-				print " [+] %s with MAC %s channel: %s encryption: %s" %(pkt.info, pkt.addr2,int( ord(pkt[Dot11Elt:3].info)), enc)
-				if pkt.info not in list_accesspoints.ssid:
-					list_accesspoints.append(accesspoint_object(pkt.info,pkt.addr2,int( ord(pkt[Dot11Elt:3].info)),enc))
-				else: duplicates.append(accesspoint_object(pkt.info,pkt.addr2,int( ord(pkt[Dot11Elt:3].info)),enc))
+				print " [+] %s with MAC %s channel: %s encryption: %s signal: %s" %(pkt.info, pkt.addr2,int( ord(pkt[Dot11Elt:3].info)), enc, signal)
+				list_accesspoints.append(accesspoint_object(pkt.info,pkt.addr2,int( ord(pkt[Dot11Elt:3].info)),enc, signal))
+				duplicates.append(accesspoint_object(pkt.info,pkt.addr2,int( ord(pkt[Dot11Elt:3].info)),enc, signal))
 				wname.append(pkt.info)
 
 
 def prino():
 	accespoint_map = {}
 	print(len(list_accesspoints))
-	
-	print(len(duplicates))
-	for i in range (len (list_accesspoints) -1):
-		for y in range (len (list_accesspoints) -1):
- 			if list_accesspoints[i].ssid == list_accesspoints[y].ssid:
- 			duplicates.append(list_accesspoints[i].ssid)
-		y+=1
-	i+=1		print(duplicates[0])
-	
-	#if it.ssid == duplicates[1] :
-		#	print('duplicate name')
-		#	print(duplicates[1])
-		#	print('accesspoint mac add in big list')
-		#	print(it.mac)
-		#print(list_accesspoints[3].ssid, list_accesspoints[3].enc)
+	unique_words = toolz.unique(list_accesspoints, key=lambda w: w.ssid)
+	for w in unique_words:
+		print(w.ssid)
+
 	
 
 	
@@ -59,17 +51,7 @@ def prino():
 	#print(duplicates)
 
 					
-			
-		#temp = pkt
-			
-		#while temp :
-		#	temp = temp.getlayer(Dot11Elt)
-		#	if temp and temp.ID == 0 and (temp.info not in aps):	
-		#		aps.add(temp.info)
-		#		print len(aps), pkt.addr3, temp.info
-		#		break
-		#	temp = temp.payload
-def channel():
+def channel():  #This will hope from channel 1 to 12
 	while True:
 		try:
 			channel = random.randrange(1,12)
@@ -102,7 +84,7 @@ if __name__ == "__main__":
 	signal.signal(signal.SIGINT, signal_handler)
 	
 
-	sniff(iface=interface, count = 400, prn = FindAps)	
+	sniff(iface=interface, count = 200, prn = FindAps)
 	#Test(wname)
 	prino()	
 	
